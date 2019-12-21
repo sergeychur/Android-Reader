ffff585453e229062d548930361c746db2993bb1
Kotyarich <ndkotov@gmail.com>
2019-12-22 02:49:39 +0300

    Auth is working

--- app/src/main/java/ru/tp_project/androidreader/MainActivity.kt
+++ app/src/main/java/ru/tp_project/androidreader/MainActivity.kt
@@ -1,16 +1,26 @@
 package ru.tp_project.androidreader
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
-import android.view.View
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -18,6 +28,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,9 +47,18 @@ class MainActivity : AppCompatActivity() {
             navController.graph
             , drawerLayout = drawer_layout
         ) // для бокового меню
+
         val toolBar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolBar)
         toolBar.setupWithNavController(navController, appBarConfiguration)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -43,5 +66,73 @@ class MainActivity : AppCompatActivity() {
         menu.findItem(R.id.action_accept).isVisible = false
         menu.findItem(R.id.action_share).isVisible = false
         return true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) { // The Task returned from this call is always completed, no need to attach
+                                   // a listener.
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                Log.w("mytag", "Google sign in failed", e)
+                updateUI(null)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d("mytag", "firebaseAuthWithGoogle:" + acct.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("mytag", "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Log.w("mytag", "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        val sideBar = findViewById<NavigationView>(R.id.nav_view)
+        if (user != null) {
+            sideBar.menu.findItem(R.id.sign_out).isVisible = true
+            sideBar.menu.findItem(R.id.sign_in).isVisible = false
+        } else {
+            sideBar.menu.findItem(R.id.sign_out).isVisible = false
+            sideBar.menu.findItem(R.id.sign_in).isVisible = true
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun signOut() {
+        auth.signOut()
+
+        googleSignInClient.signOut().addOnCompleteListener(this) {
+            updateUI(null)
+        }
+    }
+
+    fun onClick(item: MenuItem) {
+        when (item.itemId) {
+            R.id.sign_in -> signIn()
+            R.id.sign_out -> signOut()
+        }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
     }
 }
