8c4684457d83ef85f60c770ecb19e929b9be1ff8
Kotyarich <ndkotov@gmail.com>
2019-12-22 16:25:11 +0300

    Fix to mvvm, save state on rotating

--- app/src/main/java/ru/tp_project/androidreader/model/repos/UserRepository.kt
+++ app/src/main/java/ru/tp_project/androidreader/model/repos/UserRepository.kt
@@ -1,6 +1,9 @@
 package ru.tp_project.androidreader.model.repos
 
 import android.content.Context
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -10,9 +13,11 @@ import ru.tp_project.androidreader.model.AppDb
 import ru.tp_project.androidreader.model.data_models.User
 import javax.inject.Singleton
 
+
 @Singleton
 class UserRepository {
     private var currentUserID = R.integer.single_user_id
+    private val firebaseAuth = FirebaseAuth.getInstance()
 
     fun getCurrentUserID(context: Context): Int {
         return context.resources.getInteger(currentUserID)
@@ -26,6 +31,31 @@ class UserRepository {
             }
             onResult(true, user)
         }
+    }
+
+    fun firebaseSignInWithGoogle(
+        googleAuthCredential: AuthCredential,
+        onResult: (user: FirebaseUser?) -> Unit
+    ) {
+        GlobalScope.launch {
+            firebaseAuth.signInWithCredential(googleAuthCredential).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // TODO Im'not sure if we need to do smth with it
+                    // val isNew = task.result?.additionalUserInfo?.isNewUser
+                    onResult(firebaseAuth.currentUser)
+                } else {
+                    onResult(null)
+                }
+            }
+        }
+    }
+
+    fun signOut() {
+        firebaseAuth.signOut()
+    }
+
+    fun getCurrentFirebaseUser(): FirebaseUser? {
+        return firebaseAuth.currentUser
     }
 
 }
